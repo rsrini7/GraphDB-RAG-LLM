accba90386138911690207e63e94132ebba20797
Srini <7rsrini@gmail.com>
2025-04-06 11:37:35 +0530

    feat: add CSV file type support to data ingester
    The script now supports ingesting CSV files in addition to JSON files. This change allows for more flexibility in the types of data that can be processed by the data ingestion pipeline.

--- sample-data-ingester.sh
+++ sample-data-ingester.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 export PYTHONPATH="${PYTHONPATH}:$(pwd)/src"
 source "venv/bin/activate"
-python src/data_ingestion/cli.py --data-path sample_data/ --file-type json --clear-existing
+python src/data_ingestion/cli.py --data-path sample_data/ --file-type json
+python src/data_ingestion/cli.py --data-path sample_data/ --file-type csv
